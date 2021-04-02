03867182571da027e740d274143cf938e4e56022
George <george.soares509@gmail.com>
2021-04-02 14:24:11 -0300

    third-stage-concluded

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
   const [ time, setTime ] = useState(0.1 * 30);
   const [ isActive, setIsActive ] = useState(false);
   const [hasFinished, setHasFinished ] = useState(false);
@@ -33,6 +36,7 @@ export function Countdown() {
     } else if(isActive && time === 0) {
       setHasFinished(true);
       setIsActive(false);
+      startNewChallenge();
     }
   }, [isActive, time]);
 
